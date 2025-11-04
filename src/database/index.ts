1cc8bfd3f8ef2596b3b30c41c76757d58553292e
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-11-04 23:07:40 +0500

    change db paht to main

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,7 @@ import Database from "better-sqlite3";
 
 import path from "node:path";
 
-const dbPath = path.join(__dirname, "database.db");
+const dbPath = path.join(__dirname, "..", "..", "database.db");
 const db = new Database(dbPath);
 
 export default db;
